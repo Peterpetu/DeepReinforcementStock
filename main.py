90c56441b6ff0eca7229be5bdeaa166a88d37d29
Peterpetu <Petu.laht@gmail.com>
2023-06-27 16:55:28 +0200

    Updated

--- main.py
+++ main.py
@@ -27,24 +27,23 @@ def main():
         done = False
         total_reward = 0
         step_counter = 0
-        while not done:
-            # Agent takes action
-            action = agent.act(state)
-            step_counter += 1
-            if step_counter % 10 == 0:
-                print(f"Current state: {state}, Action taken: {action}")
-
-            # Get the next state, reward, and done from the environment
+        
+        for i_episode in range(1, n_episodes+1):
+            state = env.reset()
+            done = False
+            total_reward = 0
+            while not done:
+                action = agent.act(state)
+                step_counter += 1
+                if step_counter % 100 == 0:  # Only print every 100 steps
+                    print(f"Current state: {state}, Action taken: {action}")
             next_state, reward, done, _ = env.step(action)
-            # Agent learns from experience and updates Q-Network
             agent.step(state, action, reward, next_state, done)
-
-            # Update the current state
             state = next_state
-
             total_reward += reward
-            if step_counter % 10 == 0:
-                print(f"Total reward so far: {total_reward}")
+            if i_episode % 10 == 0:  # Only print every 10 episodes
+                print(f"Episode {i_episode}/{n_episodes} finished. Total reward: {total_reward}")
+
 
         # Append total reward of this episode to the list
         total_rewards.append(total_reward)
