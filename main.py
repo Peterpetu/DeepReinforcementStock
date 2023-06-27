700af3c48880b5ece5425a27eda4ab93558db400
Peterpetu <Petu.laht@gmail.com>
2023-06-27 12:10:23 +0200

    Updated files

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-from environment import StockTradingEnvironment
+from environment import Environment as StockTradingEnvironment
 from dqn_agent import DQNAgent
 import torch as torch
 
@@ -15,6 +15,9 @@ def main():
 
     # Number of episodes to train for
     n_episodes = 1000
+
+    # Initialize the list to store total rewards per episode
+    total_rewards = []
 
     for i_episode in range(1, n_episodes+1):
         # Reset the environment and the agent
@@ -38,11 +41,29 @@ def main():
 
             total_reward += reward
 
+        # Append total reward of this episode to the list
+        total_rewards.append(total_reward)
+
         # Print out some information about the training process
         print(f"Episode {i_episode}/{n_episodes} finished. Total reward: {total_reward}")
 
     # Save the trained Q-Network
     torch.save(agent.qnetwork_local.state_dict(), 'checkpoint.pth')
+
+    # Evaluate the agent's performance
+    print("Evaluating agent's performance...")
+    total_reward = 0
+    state = env.reset()
+    done = False
+    while not done:
+        action = agent.act(state)
+        next_state, reward, done = env.step(action)
+        state = next_state
+        total_reward += reward
+    print(f"Total reward: {total_reward}")
+
+    # Save the total rewards per episode to a file
+    np.save('total_rewards.npy', total_rewards)
 
 if __name__ == "__main__":
     main()
