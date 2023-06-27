71684ecc8d4d43d7fe28ad4cc93969fdbfa25495
Peterpetu <Petu.laht@gmail.com>
2023-06-27 14:04:15 +0200

    Updated files

--- model.py
+++ model.py
@@ -6,7 +6,10 @@ class QNetwork(nn.Module):
     def __init__(self, state_size, action_size, seed, hidden_layers):
         super(QNetwork, self).__init__()
         self.seed = torch.manual_seed(seed)
-        self.hidden_layers = nn.ModuleList([nn.Linear(state_size, hidden_layers[0])])
+        
+        # Adjust the input size of the first linear layer
+        self.hidden_layers = nn.ModuleList([nn.Linear(state_size*2+2, hidden_layers[0])])
+        
         layer_sizes = zip(hidden_layers[:-1], hidden_layers[1:])
         self.hidden_layers.extend([nn.Linear(h1, h2) for h1, h2 in layer_sizes])
         self.output = nn.Linear(hidden_layers[-1], action_size)
@@ -16,3 +19,4 @@ class QNetwork(nn.Module):
         for each in self.hidden_layers:
             x = F.relu(each(x))
         return self.output(x)
+
