2d5c9b9a1f1f1fdf8afd7a8b61b8b9a153c5ded9
Peterpetu <Petu.laht@gmail.com>
2023-06-27 14:23:25 +0200

    Updated files

--- environment.py
+++ environment.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 class Environment:
     def __init__(self, stock_price_history, initial_balance=10000, window_size=30):
         self.stock_price_history = np.array(stock_price_history)
@@ -34,33 +35,31 @@ class Environment:
         self.done = False
         self.current_step = 0
         return self.state
-    
+
     def get_recent_prices_volumes(self, n):
-    # Ensure stock_price_history is a 2D array
+        # Ensure stock_price_history is a 2D array
         assert len(self.stock_price_history.shape) == 2, f"Expected stock_price_history to be a 2D array, got shape {self.stock_price_history.shape}"
 
-    # Get the most recent n days' stock prices and trading volumes
+        # Get the most recent n days' stock prices and trading volumes
         recent_prices_volumes = self.stock_price_history[-n:, :2]  # Get only the first two columns (price and volume)
 
-    # Ensure the array is of the correct size
+        # Ensure the array is of the correct size
         assert recent_prices_volumes.shape == (n, 2), f"Expected shape ({n}, 2), got {recent_prices_volumes.shape}"
 
         return recent_prices_volumes
 
-
-
-
-
     @property
     def state(self):
-    # Get the most recent n days' stock prices and trading volumes
+        # Get the most recent n days' stock prices and trading volumes
         recent_prices_volumes = self.get_recent_prices_volumes(n=self.window_size)
     
-    # Construct the state vector
+        # Flatten the recent_prices_volumes array
+        recent_prices_volumes = recent_prices_volumes.flatten()
+    
+        # Construct the state vector
         state = np.concatenate((recent_prices_volumes, [self.stock_owned, self.balance]))
     
         return state
-
 
     @property
     def current_price(self):
