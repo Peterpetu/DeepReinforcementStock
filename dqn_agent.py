3c9f9fe9f7f66e873efc233336671b0518392f1a
Peterpetu <Petu.laht@gmail.com>
2023-06-27 15:18:45 +0200

    Updated

--- dqn_agent.py
+++ dqn_agent.py
@@ -79,12 +79,6 @@ class DQNAgent:
         # ------------------- update target network ------------------- #
         self.soft_update(self.qnetwork_local, self.qnetwork_target, TAU)                     
 
-    def soft_update(self, local_model, target_model, tau):
-        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
-            target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
-    
-    def reset(self):
-        self.memory.clear_memory()
 
 class ReplayBuffer:
     def __init__(self, action_size, buffer_size, batch_size, seed):
--- model.py
+++ model.py
@@ -6,10 +6,10 @@ class QNetwork(nn.Module):
     def __init__(self, state_size, action_size, seed, hidden_layers):
         super(QNetwork, self).__init__()
         self.seed = torch.manual_seed(seed)
-        
+
         # Adjust the input size of the first linear layer
-        self.hidden_layers = nn.ModuleList([nn.Linear(state_size*2+2, hidden_layers[0])])
-        
+        self.hidden_layers = nn.ModuleList([nn.Linear(state_size*30*2, hidden_layers[0])])
+
         layer_sizes = zip(hidden_layers[:-1], hidden_layers[1:])
         self.hidden_layers.extend([nn.Linear(h1, h2) for h1, h2 in layer_sizes])
         self.output = nn.Linear(hidden_layers[-1], action_size)
@@ -19,4 +19,3 @@ class QNetwork(nn.Module):
         for each in self.hidden_layers:
             x = F.relu(each(x))
         return self.output(x)
-
