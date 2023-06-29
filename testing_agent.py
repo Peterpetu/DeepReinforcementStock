650fe8eede80eca169a2cf63566939b38b716179
Peterpetu <Petu.laht@gmail.com>
2023-06-29 10:16:09 +0200

    updated

--- testing_agent.py
+++ testing_agent.py
@@ -1,5 +1,7 @@
 import numpy as np
 from environment import Environment
+from dqn_agent import DQNAgent
+import torch
 
 def load_test_data():
     # Load the testing data
@@ -11,7 +13,19 @@ def create_environment(test_data):
     env = Environment(test_data)
     return env
 
+def create_agent(env):
+    # Create the agent
+    agent = DQNAgent(state_size=env.state_size, action_size=env.action_size, seed=0, hidden_layers=[64, 64])
+    return agent
+
+def load_trained_model(agent):
+    # Load the trained model
+    agent.qnetwork_local.load_state_dict(torch.load('checkpoint.pth'))
+    print("Trained model loaded.")
+
 if __name__ == "__main__":
     test_data = load_test_data()
     env = create_environment(test_data)
-    print("Test data loaded and environment created.")
+    agent = create_agent(env)
+    load_trained_model(agent)
+    print("Agent created and trained model loaded.")
